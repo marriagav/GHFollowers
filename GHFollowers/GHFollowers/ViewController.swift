2a5ac583a38593fe2054bcfa00216ba1269f092b
Miguel Arriaga Velasco <marriagav3@gmail.com>
2024-02-28 17:07:54 -0600

    init project

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Miguel Arriaga Velasco on 28/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemCyan
+    }
+
+}
